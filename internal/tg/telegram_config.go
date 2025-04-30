af7b425f8143bea778ed7bd11897ea353565552f
c0de4un <code4un@yandex.ru>
2025-04-30 14:12:19 +0400

    #1 Implement parsing tg token from xml config

---
+++ internal/tg/telegram_config.go
@@ -0,0 +1,30 @@
+package tg
+
+import (
+	"encoding/xml"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type TelegramConfig struct {
+	XMLName xml.Name `xml:"TelegramConfig"`
+	Token   string   `xml:"Token"`
+}
+
+func LoadTelegramConfig() (*TelegramConfig, error) {
+	cfgPath := filepath.Join("config", "tg.xml")
+	content, err := os.ReadFile(cfgPath)
+	if err != nil {
+		fmt.Printf("failed to load tg config: %s", err)
+		return nil, err
+	}
+
+	model := &TelegramConfig{}
+	if err := xml.Unmarshal(content, &model); err != nil {
+		fmt.Printf("failed to parse tg config: %s", err)
+		return nil, err
+	}
+
+	return model, nil
+}
