e343f5b426d8a6b51a88bd928db28485681e5066
c0de4un <code4un@yandex.ru>
2025-05-02 18:20:27 +0400

    #16: Prompt for title

--- internal/tg/handle_client_post_editing.go
+++ internal/tg/handle_client_post_editing.go
@@ -2,11 +2,36 @@ package tg
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 	newsmodels "gitlab.com/korgi.tech/projects/go-news-tg-bot/internal/core/models"
+	"gitlab.com/korgi.tech/projects/go-news-tg-bot/internal/core/repositories"
 )
 
 func handlePostCreateStart(user *newsmodels.UserModel, uc *newsmodels.ChatModel, c *newsmodels.ClientModel, ctx context.Context, b *bot.Bot, mes models.MaybeInaccessibleMessage) {
 	// @TODO: handlePostCreateStart()
+
+	ucr := repositories.GetUserChatRepository()
+	err := ucr.SetState(ctx, uc.ID, newsmodels.CHAT_STATE_POST_TITLE)
+	if err != nil {
+		fmt.Printf("handlePostCreateStart: %v", err)
+		_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: mes.Message.Chat.ID,
+			Text:   "Failed, try again later",
+		})
+		if err != nil {
+			fmt.Printf("handlePostCreateStart: %v", err)
+		}
+
+		return
+	}
+
+	_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: mes.Message.Chat.ID,
+		Text:   "Please, enter post title",
+	})
+	if err != nil {
+		fmt.Printf("handlePostCreateStart: %v", err)
+	}
 }
