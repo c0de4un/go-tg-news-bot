5c436fb52fef37a8f2d8787e018321739783e80e
c0de4un <code4un@yandex.ru>
2025-05-22 18:55:38 +0400

    #26 Implement get forward post by status

--- internal/core/repositories/forward_post_repository.go
+++ internal/core/repositories/forward_post_repository.go
@@ -75,6 +75,25 @@ func (r *ForwardPostRepository) Create(
 	return r.fillModelByRow(row, post)
 }
 
+func (r *ForwardPostRepository) GetByStatus(status int64) (*models.ForwardPostModel, error) {
+	dbm, _ := database.GetDBManager()
+	if dbm == nil {
+		fmt.Println("ForwardPostRepository.GetByStatus: dbm is nil, maybe app is terminating")
+		return nil, fmt.Errorf("ForwardPostRepository.GetByStatus: dbm is nil, maybe app is terminating")
+	}
+
+	db := dbm.GetDBConnection()
+	query := `
+        SELECT *
+        FROM forward_posts 
+        WHERE status = $1
+        LIMIT 1`
+
+	row := db.QueryRow(query, status)
+
+	return r.fillModelByRow(row, nil)
+}
+
 func (r *ForwardPostRepository) fillModelByRow(row *sql.Row, post *models.ForwardPostModel) (*models.ForwardPostModel, error) {
 	if post == nil {
 		post = &models.ForwardPostModel{}
